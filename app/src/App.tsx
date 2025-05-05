e7eedbed8d1255c5707e48c493d6837852ba9f5f
Daniel Glasgow <daniel@harlus.com>
2025-05-05 19:45:48 -0400

    Remove unused files

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
 import WorkspaceSelect from "./pages/WorkspaceSelect";
 import WorkspaceNew from "./pages/WorkspaceNew";
 import NotFound from "./pages/NotFound";
@@ -20,7 +19,6 @@ const App = () => (
           <Route path="/" element={<WorkspaceSelect />} />
           <Route path="/workspace/:workspaceId" element={<Workspace />} />
           <Route path="/workspace/new" element={<WorkspaceNew />} />
-          <Route path="/workspaceOld/:workspaceId" element={<Index />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </HashRouter>
