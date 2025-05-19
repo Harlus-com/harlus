060626b91cadc9ea8919ce50a47aa223700881f9
OctaveOliviers <octave.oliviers@gmail.com>
2025-05-19 18:43:37 +0200

    removed error in setup.py

--- ml/tools/knowledge_graph/setup.py
+++ ml/tools/knowledge_graph/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="harlus_contrast_tool",
+    name="harlus_knowledge_graph",
     version="0.1.0",  # DO NOT BUMP THIS VERSION (see python/env/README.md)
     packages=find_packages(where="src"),
     package_dir={"": "src"},
