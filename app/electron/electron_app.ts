c96ccf82df4b4e8c89b32778efc4fd895064f443
Daniel Glasgow <daniel@harlus.com>
2025-05-20 08:25:18 -0400

    Use same code for production and dev electron app (#86)

---
+++ app/electron/electron_app.ts
@@ -0,0 +1,140 @@
+import fs from "fs";
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import https from "https";
+import express from "express";
+import { Agent as UndiciAgent } from "undici";
+import { ElectronAppState } from "./electron_types";
+import { setupIpcHandlers } from "./ipc";
+
+export abstract class ElectronApp {
+  abstract runPrescripts(): void;
+
+  abstract getBaseUrl(): string;
+
+  abstract getTlsDir(): string;
+
+  abstract startUiServer(): Promise<void>;
+
+  abstract onWindowReady(mainWindow: BrowserWindow): void;
+
+  start() {
+    this.runPrescripts();
+    const baseUrl = this.getBaseUrl();
+    const tlsDir = this.getTlsDir();
+    const httpsAgent = new https.Agent({
+      cert: fs.readFileSync(path.join(tlsDir, "client.crt")),
+      key: fs.readFileSync(path.join(tlsDir, "client.key")),
+      ca: fs.readFileSync(path.join(tlsDir, "ca.crt")),
+    });
+    const httpsDispatcher = new UndiciAgent({
+      connect: {
+        ca: fs.readFileSync(path.join(tlsDir, "ca.crt")),
+        cert: fs.readFileSync(path.join(tlsDir, "client.crt")),
+        key: fs.readFileSync(path.join(tlsDir, "client.key")),
+      },
+    });
+
+    app.whenReady().then(async () => {
+      await this.startUiServer();
+      const mainWindow = createWindow();
+      this.onWindowReady(mainWindow);
+      const state: ElectronAppState = {
+        mainWindow,
+        baseUrl,
+        httpsAgent,
+        httpsDispatcher,
+        eventSources: new Map(),
+      };
+      setupIpcHandlers(state);
+    });
+
+    function createWindow() {
+      const mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        minWidth: 800,
+        minHeight: 400,
+        webPreferences: {
+          preload: path.join(__dirname, "preload.cjs"),
+          contextIsolation: true,
+          nodeIntegration: false,
+        },
+      });
+      mainWindow.loadURL("http://localhost:8080");
+      return mainWindow;
+    }
+  }
+}
+
+export class ProductionElectronApp extends ElectronApp {
+  runPrescripts(): void {
+    const logPath = "/tmp/harlus.txt"; // TODO: Put this somewhere more acceptable and cross-platform enabled
+    const logStream = fs.createWriteStream(logPath, { flags: "a" });
+    console.log = (...args) => {
+      logStream.write(`[LOG] ${args.join(" ")}\n`);
+    };
+    console.error = (...args) => {
+      logStream.write(`[ERROR] ${args.join(" ")}\n`);
+    };
+  }
+  getBaseUrl(): string {
+    return "https://harlus-api-dev.eastus.azurecontainer.io";
+  }
+  getTlsDir(): string {
+    return path.join(process.resourcesPath, "tls");
+  }
+  startUiServer(): Promise<void> {
+    return new Promise((resolve) => {
+      const DIST = path.join(__dirname, "../dist");
+      const httpApp = express();
+      // Serve all files in your dist folder
+      httpApp.use(express.static(DIST));
+      // For any other route, serve index.html (SPA client‐side routing)
+      httpApp.get("/*", (_req: any, res: any) => {
+        res.sendFile(path.join(DIST, "index.html"));
+      });
+      httpApp.listen(8080, () => {
+        console.log(`▶️  HTTP server listening on http://localhost:8080`);
+        resolve();
+      });
+    });
+  }
+  onWindowReady(mainWindow: BrowserWindow): void {
+    // pass
+  }
+}
+
+export class DevElectronApp extends ElectronApp {
+  runPrescripts(): void {
+    // pass
+  }
+  getBaseUrl(): string {
+    const args = process.argv;
+    const useRemoteServer = !!args.find((arg) => arg === "--remote-server");
+    const useLocalDockerSsl = !!args.find((arg) => arg === "--local-docker");
+    if (useLocalDockerSsl) {
+      console.log(
+        "Using local docker with SSL, relying on /etc/hosts to map harlus-api-dev.eastus.azurecontainer.io to localhost"
+      );
+      return "https://harlus-api-dev.eastus.azurecontainer.io:8000";
+    }
+    if (useRemoteServer) {
+      console.log(
+        "Using remote server, make sure /etc/hosts does not override harlus-api-dev.eastus.azurecontainer.io"
+      );
+      return "https://harlus-api-dev.eastus.azurecontainer.io";
+    }
+    return "http://127.0.0.1:8000";
+  }
+  getTlsDir(): string {
+    const projectRoot = path.resolve(__dirname, "../../");
+    return path.join(projectRoot, "infra/nginx-mtls/tls");
+  }
+  startUiServer(): Promise<void> {
+    return Promise.resolve();
+  }
+  onWindowReady(mainWindow: BrowserWindow): void {
+    mainWindow.webContents.openDevTools();
+  }
+}
---
+++ app/electron/ipc.ts
@@ -0,0 +1,154 @@
+import fs from "fs";
+import { ipcMain, dialog } from "electron";
+import mime from "mime";
+import axios from "axios";
+import { EventSource } from "eventsource";
+import { ElectronAppState } from "./electron_types";
+import { Uploader } from "./upload";
+
+export function setupIpcHandlers(electronAppState: ElectronAppState) {
+  const { mainWindow, baseUrl, httpsAgent, httpsDispatcher, eventSources } =
+    electronAppState;
+
+  // TODO: Clean this up we're not using it
+  ipcMain.handle("open-file-dialog", async () => {
+    const { filePaths } = await dialog.showOpenDialog(mainWindow, {
+      properties: ["openFile", "multiSelections"],
+      filters: [{ name: "PDF Documents", extensions: ["pdf"] }],
+    });
+
+    return filePaths;
+  });
+
+  ipcMain.handle("get-file-stats", async (_, filePath) => {
+    try {
+      const stats = await fs.promises.stat(filePath);
+      return {
+        path: filePath,
+        size: stats.size,
+        mtime: stats.mtime,
+        ctime: stats.ctime,
+        birthtime: stats.birthtime,
+        isDirectory: stats.isDirectory(),
+        mimeType: mime.getType(filePath) || null,
+      };
+    } catch (error) {
+      console.error("Error getting file stats:", error);
+      return null;
+    }
+  });
+
+  ipcMain.handle("get-file-content", async (_, filePath) => {
+    try {
+      console.log("getting file content", filePath);
+      const content = await fs.promises.readFile(filePath);
+      return content;
+    } catch (error) {
+      console.error("Error reading file:", error);
+      return null;
+    }
+  });
+
+  ipcMain.handle("server-get", async (_, path: string, authHeader: string) => {
+    const url = `${baseUrl}${path}`;
+    const response = await axios.get(url, {
+      httpsAgent,
+      headers: {
+        Authorization: authHeader,
+      },
+    });
+    return response.data;
+  });
+
+  ipcMain.handle(
+    "server-get-buffer",
+    async (_, path: string, authHeader: string) => {
+      const url = `${baseUrl}${path}`;
+      const response = await axios.get(url, {
+        httpsAgent,
+        responseType: "arraybuffer",
+        headers: {
+          Authorization: authHeader,
+        },
+      });
+      return response.data;
+    }
+  );
+
+  ipcMain.handle(
+    "server-post",
+    async (_, path: string, body: any, authHeader: string) => {
+      const url = `${baseUrl}${path}`;
+      const response = await axios.post(url, body, {
+        httpsAgent,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: authHeader,
+        },
+      });
+      return response.data;
+    }
+  );
+
+  ipcMain.handle(
+    "server-delete",
+    async (_, path: string, authHeader: string) => {
+      const url = `${baseUrl}${path}`;
+      const response = await axios.delete(url, {
+        httpsAgent,
+        headers: {
+          Authorization: authHeader,
+        },
+      });
+      return response.data;
+    }
+  );
+
+  ipcMain.handle(
+    "server-upload",
+    (_, filePath: string, workspaceId: string, authHeader: string) => {
+      return new Uploader(electronAppState).upload(
+        filePath,
+        workspaceId,
+        authHeader
+      );
+    }
+  );
+
+  ipcMain.handle("get-base-url", () => {
+    return baseUrl;
+  });
+
+  ipcMain.handle("add-event-listener", async (_, eventSourceId, eventName) => {
+    const eventSource = eventSources.get(eventSourceId);
+    if (eventSource) {
+      eventSource.addEventListener(eventName, (event: any) => {
+        mainWindow.webContents.send("event-forwarder", {
+          eventSourceId,
+          type: eventName,
+          data: event.data,
+        });
+      });
+    }
+  });
+
+  const fetchWithMtls = (url: string | URL, init: any) =>
+    fetch(url, { ...init, dispatcher: httpsDispatcher });
+
+  ipcMain.handle("create-event-source", async (_, url) => {
+    const eventSourceId = Math.random().toString(36).substring(7);
+    const eventSource = new EventSource(url, {
+      fetch: fetchWithMtls,
+    });
+    eventSources.set(eventSourceId, eventSource);
+    return eventSourceId;
+  });
+
+  ipcMain.handle("close-event-source", async (_, eventSourceId) => {
+    const eventSource = eventSources.get(eventSourceId);
+    if (eventSource) {
+      eventSource.close();
+      eventSources.delete(eventSourceId);
+    }
+  });
+}
