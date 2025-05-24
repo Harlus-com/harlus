b12c8f32d5c57962dc8b6a67ebad6c6c25edb526
Daniel Glasgow <daniel@harlus.com>
2025-05-24 08:07:55 -0400

    Small chat source fixes

--- ml/tools/workspace_chat/src/harlus_workspace_chat/chat_source_comments.py
+++ ml/tools/workspace_chat/src/harlus_workspace_chat/chat_source_comments.py
@@ -1,65 +1,61 @@
-from .custom_types import(
-    ChatSourceComment,
-    HighlightArea,
-    DocSearchRetrievedNode
-)
+from .custom_types import ChatSourceComment, HighlightArea, DocSearchRetrievedNode
 import uuid
 from llama_index.core.schema import NodeWithScore
 from typing import List, Tuple
 from harlus_source_highlight import HighlightPipeline
 
+
 def get_chat_source_comments_from_retrieved_nodes(
-        retrieved_nodes: list[DocSearchRetrievedNode], 
-        thread_id: str, 
-        message_id: str,
-        ) -> list[ChatSourceComment]:
+    retrieved_nodes: list[DocSearchRetrievedNode],
+    thread_id: str,
+    message_id: str,
+) -> list[ChatSourceComment]:
     chat_source_comments = []
     for retrieved_node in retrieved_nodes:
         highlight_area = HighlightArea(
             bounding_boxes=retrieved_node.metadata.bounding_boxes,
-            jump_to_page_number=retrieved_node.metadata.page_nb
+            jump_to_page_number=retrieved_node.metadata.page_nb,
         )
         chat_source_comment = ChatSourceComment(
             id=str(uuid.uuid4()),
             highlight_area=highlight_area,
             file_id=retrieved_node.metadata.file_id,
             text="Source used in chat",
-            next_chat_comment_id=str(uuid.uuid4()), # currently not used
+            next_chat_comment_id=str(uuid.uuid4()),  # currently not used
             thread_id=thread_id,
             message_id=message_id,
         )
         chat_source_comments.append(chat_source_comment)
     return chat_source_comments
-        
+
 
 async def get_chat_source_comments_from_citations(
-        citations: List[Tuple[int, str]], 
-        retrieved_nodes: list[NodeWithScore],
-        file_id_to_path: dict[str, str],
-        thread_id: str,
-        message_id: str
-        ) -> list[ChatSourceComment]:
-    
+    citations: List[Tuple[int, str]],
+    retrieved_nodes: list[NodeWithScore],
+    file_id_to_path: dict[str, str],
+    thread_id: str,
+    message_id: str,
+) -> list[ChatSourceComment]:
+
     chat_source_comments = []
     highlight_pipeline = HighlightPipeline(
-            nodes=retrieved_nodes,
-            file_id_to_path=file_id_to_path,
-        )
+        nodes=retrieved_nodes,
+        file_id_to_path=file_id_to_path,
+    )
     for citation in citations:
         citation_nb = citation[0]
         citation_text = citation[1]
         highlight_area, file_id, state = await highlight_pipeline.run(citation_text)
+        if highlight_area is None:
+            continue
         chat_source_comment = ChatSourceComment(
             id=str(uuid.uuid4()),
             highlight_area=HighlightArea(**highlight_area.model_dump()),
             file_id=file_id,
             text=f"[{citation_nb}]",
-            next_chat_comment_id=str(uuid.uuid4()), # currently not used
+            next_chat_comment_id=str(uuid.uuid4()),  # currently not used
             thread_id=thread_id,
             message_id=message_id,
         )
         chat_source_comments.append(chat_source_comment)
     return chat_source_comments
-
-        
-             
--- ml/tools/workspace_chat/src/harlus_workspace_chat/tool_executor.py
+++ ml/tools/workspace_chat/src/harlus_workspace_chat/tool_executor.py
@@ -3,11 +3,11 @@ from langchain_core.messages import (
     AIMessageChunk,
 )
 from typing import (
-    AsyncIterator, 
+    AsyncIterator,
 )
-import json 
+import json
 from .custom_types import (
-    DocSearchRetrievedNode, 
+    DocSearchRetrievedNode,
     TavilyToolRetrievedWebsite,
 )
 from langgraph.config import get_stream_writer
@@ -21,41 +21,50 @@ from .utils import (
     get_last_message,
 )
 
+
 class ToolExecutorNode:
-    
-    def __init__(self, 
-                 tools: list, 
-                 tool_name_to_metadata: dict, 
-                 message_state_key: str = "messages", 
-                 retrieved_items_state_key: str = "retrieved_items",
-                 file_id_to_path: dict[str, str] = {}
-                 ) -> None:    
+
+    def __init__(
+        self,
+        tools: list,
+        tool_name_to_metadata: dict,
+        message_state_key: str = "messages",
+        retrieved_items_state_key: str = "retrieved_items",
+        file_id_to_path: dict[str, str] = {},
+    ) -> None:
         self.tools_by_name = {tool.name: tool for tool in tools}
         self.tool_name_to_metadata = tool_name_to_metadata
         self.message_state_key = message_state_key
         self.retrieved_items_state_key = retrieved_items_state_key
         # TODO: add these as a property of the DocSearchToolWrapper
         self.doc_search_tool_types = [
-            "doc_search_semantic_query_engine", 
-            "doc_search_summary_query_engine", 
-            "doc_search_semantic_retriever", 
+            "doc_search_semantic_query_engine",
+            "doc_search_summary_query_engine",
+            "doc_search_semantic_retriever",
             "doc_search_summary_retriever",
-            "doc_search_sub_question_semantic_query_engine"
+            "doc_search_sub_question_semantic_query_engine",
         ]
         self.file_id_to_path = file_id_to_path
-    async def _process_doc_search_tool_call(self, tool_call) -> tuple[list[DocSearchRetrievedNode], ToolMessage]:
+
+    async def _process_doc_search_tool_call(
+        self, tool_call
+    ) -> tuple[list[DocSearchRetrievedNode], ToolMessage]:
         retrieved_nodes = []
         tool = self.tools_by_name[tool_call["name"]]
         tool_metadata = self.tool_name_to_metadata[tool_call["name"]]
         tool_type = tool_metadata.get("type", "")
         tool_friendly_name = tool_metadata.get("friendly_name", "")
         writer = get_stream_writer()
-        writer({"reading_message" : f"Reading {tool_friendly_name} ... "})
+        writer({"reading_message": f"Reading {tool_friendly_name} ... "})
         print(f" - executing tool call {tool_call['name']}")
         tool_result = await tool.ainvoke(tool_call["args"])
-        nodes_with_scores = get_nodes_with_score_from_doc_search_tool_result(tool_result, tool_type)
+        nodes_with_scores = get_nodes_with_score_from_doc_search_tool_result(
+            tool_result, tool_type
+        )
         for node_with_score in nodes_with_scores:
-            retrieved_node = get_doc_search_retrieved_node_from_node_with_score(node_with_score, self.file_id_to_path)
+            retrieved_node = get_doc_search_retrieved_node_from_node_with_score(
+                node_with_score, self.file_id_to_path
+            )
             retrieved_nodes.append(retrieved_node)
         content = json.dumps(tool_result.content)
         tool_message = ToolMessage(
@@ -64,8 +73,10 @@ class ToolExecutorNode:
             tool_call_id=tool_call["id"],
         )
         return retrieved_nodes, tool_message
-    
-    async def _process_tavily_search_tool_call(self, tool_call) -> tuple[list[TavilyToolRetrievedWebsite], ToolMessage]:
+
+    async def _process_tavily_search_tool_call(
+        self, tool_call
+    ) -> tuple[list[TavilyToolRetrievedWebsite], ToolMessage]:
         tool = self.tools_by_name[tool_call["name"]]
         tool_metadata = self.tool_name_to_metadata[tool_call["name"]]
         tool_friendly_name = tool_metadata.get("friendly_name", "")
@@ -75,7 +86,9 @@ class ToolExecutorNode:
         content = json.dumps(tool_result)
         tavily_tool_retrieved_websites = []
         for result in tool_result.get("results", []):
-            tavily_tool_retrieved_website = get_tavily_tool_retrieved_website_from_tool_result(result)
+            tavily_tool_retrieved_website = (
+                get_tavily_tool_retrieved_website_from_tool_result(result)
+            )
             tavily_tool_retrieved_websites.append(tavily_tool_retrieved_website)
         tool_message = ToolMessage(
             content=content,
@@ -83,7 +96,7 @@ class ToolExecutorNode:
             tool_call_id=tool_call["id"],
         )
         return tavily_tool_retrieved_websites, tool_message
-    
+
     def _parse_tool_call_type(self, tool_call) -> str:
         tool = self.tools_by_name[tool_call["name"]]
         tool_metadata = self.tool_name_to_metadata[tool_call["name"]]
@@ -95,36 +108,37 @@ class ToolExecutorNode:
         else:
             raise ValueError(f" - {tool} is not a recognized tool.")
 
-    async def __call__(self, state: dict) -> AsyncIterator[dict | AIMessageChunk]:  
+    async def __call__(self, state: dict) -> AsyncIterator[dict | AIMessageChunk]:
         message = get_last_message(state, self.message_state_key)
         tool_messages = []
         retrieved_items = []
-        
 
         tasks = []
         for tool_call in message.tool_calls:
             tool_type = self._parse_tool_call_type(tool_call)
-            
+
             if tool_type == "doc_search":
                 tasks.append(self._process_doc_search_tool_call(tool_call))
             elif tool_type == "tavily_search":
                 tasks.append(self._process_tavily_search_tool_call(tool_call))
-        
+
         results = await asyncio.gather(*tasks)
-        
+
         for result in results:
-            if isinstance(result[0], list) and isinstance(result[0][0], DocSearchRetrievedNode):
-                retrieved_nodes, tool_message = result
+            retrieved_data, tool_message = result
+            if not isinstance(retrieved_data, list):
+                continue
+            retrieved_data_is_nodes = all(
+                isinstance(data, DocSearchRetrievedNode) for data in retrieved_data
+            )
+            retrieved_data_is_websites = all(
+                isinstance(data, TavilyToolRetrievedWebsite) for data in retrieved_data
+            )
+            if retrieved_data_is_nodes or retrieved_data_is_websites:
                 tool_messages.append(tool_message)
-                retrieved_items.extend(retrieved_nodes)
-            elif isinstance(result[0], list) and isinstance(result[0][0], TavilyToolRetrievedWebsite):
-                tavily_tool_retrieved_websites, tool_message = result
-                tool_messages.append(tool_message)
-                retrieved_items.extend(tavily_tool_retrieved_websites)
-        
+                retrieved_items.extend(retrieved_data)
+
         yield {
             self.message_state_key: tool_messages,
             self.retrieved_items_state_key: retrieved_items,
         }
-        
-
